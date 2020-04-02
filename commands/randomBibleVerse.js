3b9bb1cd7bfcf6e83829e3dd4e3dd81c1e47382e
TheGreatestPretender <katschmidt201@gmail.com>
2020-04-02 00:20:46 -0400

    updated things

--- commands/randomBibleVerse.js
+++ commands/randomBibleVerse.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'jesus',
     description: 'RNJesus will quote a random bible verse',
     execute(message) {
-        message.channel.send('this is your god talking, also known as BIDO!', {tts: true});
+        message.channel.send('For Lorenzi loved his discord chat so much he made everyone a hazmat suit in the time of the rona #blessed')
+        //message.channel.send('this is your god talking, also known as BIDO!', {tts: true});
     }
 }
--- commands/twoChoiceFood.js
+++ commands/twoChoiceFood.js
@@ -5,14 +5,15 @@ module.exports = {
     execute(message, args) {
         let rng = Math.floor(Math.random() * 10) + 1;
 
-        if (rng % 2 === 0 && !8) {
-            message.channel.send(`You should choose ${args[1]}`);
-        }
-        else if (rng === 8) {
-            message.channel.send('Sorry try again :)');
-        }
-        else {
-            message.channel.send(`You should choose ${args[0]}`);
+        if (args.includes('or')) {
+            
+            if (rng % 2 === 0) {
+                if (rng === 8) message.channel.send('Sorry try again :)')
+                else message.channel.send(`You should choose ${args[1]}`);
+            }
+            else {
+                message.channel.send(`You should choose ${args[0]}`);
+            }
         }
     }
 }
