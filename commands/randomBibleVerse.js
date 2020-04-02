57f6a99314f02e0b95f433f97f3a6c37995d0a93
TheGreatestPretender <katschmidt201@gmail.com>
2020-04-01 23:43:10 -0400

    just added some tts stuff

--- commands/randomBibleVerse.js
+++ commands/randomBibleVerse.js
@@ -1,14 +1,7 @@
 module.exports = {
-    name: 'Jesus',
+    name: 'jesus',
     description: 'RNJesus will quote a random bible verse',
-    args: true,
     execute(message) {
-        if (message.member.voice.channel) {
-            const con = message.member.voice.channel.join();
-            const dispatch = con.play('https://www.youtube.com/watch?v=7ODcC5z6Ca0');
-    
-            dispatch.on('start', () => { console.log('Jesus has entered...')})
-        }
-        
+        message.channel.send('this is your god talking, also known as BIDO!', {tts: true});
     }
 }
