92201df054a4336a46b7b15e136767acdcd06d63
Kat Schmidt <katschmidt201@gmail.com>
2020-04-17 00:36:09 -0400

    fixed logic

--- commands/twoChoiceFood.js
+++ commands/twoChoiceFood.js
@@ -3,16 +3,21 @@ module.exports = {
     description: 'just an rng for choosing two options of food',
     args: true,
     execute(message, args) {
+        const baddies = ['or', 'OR']
+
         let rng = Math.floor(Math.random() * 10) + 1;
 
-        if (rng % 2 === 0 && !8) {
-            message.channel.send(`You should choose ${args[1]}`);
-        }
-        else if (rng === 8) {
-            message.channel.send('Sorry try again :)', { files: ['./media/tryAgainImg.png']});
-        }
-        else {
-            message.channel.send(`You should choose ${args[0]}`);
+        if (args.includes(baddies) || !args.includes(baddies)){
+            if (rng % 2 === 0) {
+                if (rng === 8) {
+                    message.channel.send('Sorry try again :)', { files: ['./media/tryAgainImg.png']});
+                    return;
+                }
+                message.channel.send(`You should choose ${args[2]}`);
+            }
+            else {
+                message.channel.send(`You should choose ${args[0]}`);
+            }
         }
     }
 }
